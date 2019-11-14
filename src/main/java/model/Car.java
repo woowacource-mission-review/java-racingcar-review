e569c9560059d46aaf7de61bf4d6640ecccc664e
skygl <jupiter9501@gmail.com>
2019-11-14 16:11:52 +0900

    feat: 자동차 위치 추가

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -4,13 +4,20 @@ import model.exception.CarCreateException;
 
 public class Car {
     private String name;
+    private Integer position;
 
     public Car(String name) {
-        validate(name);
-        this.name = name;
+        this(name, 0);
     }
 
-    private void validate(String name) {
+    public Car(String name, Integer position) {
+        validateName(name);
+        validatePosition(position);
+        this.name = name;
+        this.position = position;
+    }
+
+    private void validateName(String name) {
         validateNull(name);
         validateEmpty(name);
     }
@@ -25,5 +32,15 @@ public class Car {
         if ("".equals(name)) {
             throw new CarCreateException("차 이름에 비어있는 문자열을 입력할 수 없습니다.");
         }
+    }
+
+    private void validatePosition(Integer position) {
+        if (position == null) {
+            throw new CarCreateException("위치에 NULL을 입력할 수 없습니다.");
+        }
+    }
+
+    public boolean matchPosition(Integer position) {
+        return this.position.equals(position);
     }
 }
--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -4,6 +4,7 @@ import model.exception.CarCreateException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -15,14 +16,40 @@ public class CarTest {
     }
 
     @Test
-    @DisplayName("빈 문자열 입력시 차 생성 에러")
-    void car_create_empty_string() {
+    @DisplayName("기본 위치가 0인지 비교")
+    void default_position_compare() {
+        Car car = new Car("ABC");
+        assertThat(car.matchPosition(0)).isTrue();
+    }
+
+    @Test
+    @DisplayName("포지션 입력 차 생성")
+    void car_create_with_position() {
+        assertDoesNotThrow(() -> new Car("ABC", 2));
+    }
+
+    @Test
+    @DisplayName("특정 위치로 생성할 경우 위치 비교")
+    void specific_position_compare() {
+        Car car = new Car("ABC", 2);
+        assertThat(car.matchPosition(2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("이름에 빈 문자열 입력시 차 생성 에러")
+    void car_create_empty_string_name() {
         assertThrows(CarCreateException.class, () -> new Car(""));
     }
 
     @Test
-    @DisplayName("NULL 입력시 차 생성 에러")
-    void car_create_null() {
+    @DisplayName("이름에 NULL 입력시 차 생성 에러")
+    void car_create_null_name() {
         assertThrows(CarCreateException.class, () -> new Car(null));
+    }
+
+    @Test
+    @DisplayName("위치에 NULL 입력시 차 생성 에러")
+    void car_create_null_position() {
+        assertThrows(CarCreateException.class, () -> new Car("ABC", null));
     }
 }
