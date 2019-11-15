e08c9b40613e80eef9f7bd8848b3301ca2de61b1
skygl <jupiter9501@gmail.com>
2019-11-15 13:22:02 +0900

    feat: 기본 자동차 이동 전략에서 범위 밖 입력시 에러 발생

--- src/main/java/model/DefaultRandomMoveStrategy.java
+++ src/main/java/model/DefaultRandomMoveStrategy.java
@@ -3,6 +3,9 @@ package model;
 public class DefaultRandomMoveStrategy implements MoveStrategy {
     @Override
     public boolean canMove(int seed) {
+        if (seed < 0 || seed > 9) {
+            throw new IllegalArgumentException("랜덤 값은 0 ~ 9 사이의 정수이어야 합니다.");
+        }
         return seed >= 4;
     }
 }
--- src/test/java/model/DefaultRandomMoveStrategyTest.java
+++ src/test/java/model/DefaultRandomMoveStrategyTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DefaultRandomMoveStrategyTest {
     private DefaultRandomMoveStrategy moveStrategy = new DefaultRandomMoveStrategy();
@@ -18,5 +19,17 @@ public class DefaultRandomMoveStrategyTest {
     @DisplayName("4 미만 입력시 이동 불가능")
     void under_4_cant_movable() {
         assertThat(moveStrategy.canMove(3)).isFalse();
+    }
+
+    @Test
+    @DisplayName("0 미만 입력시 에러")
+    void under_0_error() {
+        assertThrows(IllegalArgumentException.class, () -> moveStrategy.canMove(-1));
+    }
+
+    @Test
+    @DisplayName("10 이상 입력시 에러")
+    void over_10_error() {
+        assertThrows(IllegalArgumentException.class, () -> moveStrategy.canMove(10));
     }
 }
