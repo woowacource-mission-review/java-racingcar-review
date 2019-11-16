aea8a4791ad469fbb3a12b9bc750b8bd85185e29
skygl <jupiter9501@gmail.com>
2019-11-16 16:36:42 +0900

    feat: 라운드 클래스 Iterable 인터페이스 구현

--- src/main/java/model/Round.java
+++ src/main/java/model/Round.java
@@ -2,7 +2,9 @@ package model;
 
 import model.exception.InvalidRoundException;
 
-class Round {
+import java.util.Iterator;
+
+class Round implements Iterable<Integer> {
     private int round;
 
     Round(int round) {
@@ -14,5 +16,23 @@ class Round {
         if (round < 1) {
             throw new InvalidRoundException("라운드는 1이상의 정수여야 합니다.");
         }
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            int currentRound = 0;
+            int maxRound = round;
+
+            @Override
+            public boolean hasNext() {
+                return currentRound < maxRound;
+            }
+
+            @Override
+            public Integer next() {
+                return ++currentRound;
+            }
+        };
     }
 }
--- src/test/java/model/RoundTest.java
+++ src/test/java/model/RoundTest.java
@@ -4,6 +4,7 @@ import model.exception.InvalidRoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -18,5 +19,18 @@ public class RoundTest {
     @DisplayName("1 미만의 정수 입력시 에러")
     void under_1_error() {
         assertThrows(InvalidRoundException.class, () -> new Round(0));
+    }
+
+    @Test
+    @DisplayName("Iterable 반복 수 테스트")
+    void iterable() {
+        int roundCount = 5;
+        Round round = new Round(roundCount);
+
+        int iter = 0;
+        for (Integer i : round) {
+            ++iter;
+        }
+        assertThat(iter).isEqualTo(roundCount);
     }
 }
