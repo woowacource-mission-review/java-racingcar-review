ce5aa02d21907eceae49621fe4d9fefaac22ffb1
green4469 <green4469@gmail.com>
2019-11-17 20:19:34 +0900

    feat: proceed race one time, go or stop each car

--- src/main/java/racing/controller/RacingGame.java
+++ src/main/java/racing/controller/RacingGame.java
@@ -1,22 +1,29 @@
 package racing.controller;
 
 import racing.model.RacingCar;
+import racing.model.RacingCarProceedStrategy;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class RacingGame {
     private final List<RacingCar> cars;
-    private final int numOfRaces;
+    private final RacingCarProceedStrategy proceedStrategy;
 
-    public RacingGame(List<String> carNames, int numOfRaces) {
+    public RacingGame(List<String> carNames, RacingCarProceedStrategy proceedStrategy) {
         this.cars = carNames.stream()
                 .map(RacingCar::new)
                 .collect(Collectors.toList());
-        this.numOfRaces = numOfRaces;
+        this.proceedStrategy = proceedStrategy;
     }
 
     public List<RacingCar> getCars() {
         return this.cars;
+    }
+
+    public void proceed() {
+        for (RacingCar car : cars) {
+            car.goForward(proceedStrategy);
+        }
     }
 }
--- src/test/java/racing/controller/RacingGameTest.java
+++ src/test/java/racing/controller/RacingGameTest.java
@@ -7,13 +7,23 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class RacingGameTest {
     @DisplayName("이름 리스트를 통해 레이싱 카들을 생성한다.")
     @Test
-    void createRacingCarsFromNamesList() {
+    void createRacingCarsFromNamesListTest() {
         List<String> carNames = Lists.newArrayList("moomin", "baedi", "comac", "JM", "EZ");
-        int numOfRaces = 5;
-        RacingGame racingGame = new RacingGame(carNames, numOfRaces);
+        RacingGame racingGame = new RacingGame(carNames, () -> true);
         Assertions.assertNotNull(racingGame.getCars());
+    }
+
+    @DisplayName("레이싱 카들을 전진 또는 멈춘다.")
+    @Test
+    void proceedRacingTest() {
+        List<String> carNames = Lists.newArrayList("moomin", "baedi", "comac", "JM", "EZ");
+        RacingGame racingGame = new RacingGame(carNames, () -> true);
+        racingGame.proceed();
+        assertThat(racingGame.getCars().stream().filter(car -> car.getPosition() == 1).count()).isEqualTo(carNames.size());
     }
 }
