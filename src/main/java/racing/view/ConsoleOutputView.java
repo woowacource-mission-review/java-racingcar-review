f192c4149dd9bbc58854448c4c7b42648e17a5be
green4469 <green4469@gmail.com>
2019-11-17 21:15:59 +0900

    chore: modify view presentation

--- src/main/java/racing/view/ConsoleOutputView.java
+++ src/main/java/racing/view/ConsoleOutputView.java
@@ -8,13 +8,14 @@ import java.util.stream.Collectors;
 public class ConsoleOutputView {
     public static void printRace(List<RacingCar> cars) {
         cars.forEach(car -> System.out.println(String.format("%s : %s", car.getName(), getDashes(car))));
+        System.out.println();
     }
 
     public static void printWinners(List<RacingCar> winners) {
         String winnersNames = winners.stream()
                 .map(RacingCar::getName)
                 .collect(Collectors.joining(", "));
-        System.out.println(String.format("%s가 최종 우승했습니다.", winnersNames));
+        System.out.println(String.format("%s이(가) 최종 우승했습니다.", winnersNames));
     }
 
     private static String getDashes(RacingCar car) {
