3d2e71c5d436181ea0d77dbb23014b22020953c3
green4469 <green4469@gmail.com>
2019-11-17 21:15:27 +0900

    feat: add racing app main method

--- src/main/java/racing/RacingApp.java
+++ src/main/java/racing/RacingApp.java
@@ -1,7 +1,29 @@
 package racing;
 
+import racing.controller.RacingGame;
+import racing.model.RacingCarProceedStrategy;
+import racing.model.RandomBasedRacingStrategy;
+import racing.model.RandomIntegerGenerator;
+import racing.model.ResultStatistics;
+import racing.view.ConsoleInputView;
+import racing.view.ConsoleOutputView;
+
+import java.util.List;
+
 public class RacingApp {
     public static void main(String[] args) {
+        List<String> carNames = ConsoleInputView.inputCarNames();
+        int numOfRaces = ConsoleInputView.inputNumOfRaces();
+        RacingCarProceedStrategy strategy = new RandomBasedRacingStrategy(new RandomIntegerGenerator(0, 9), 4);
+        RacingGame racingGame = new RacingGame(carNames, strategy);
 
+        System.out.println("\n실행결과");
+        for (int i = 0; i < numOfRaces; i++) {
+            racingGame.proceed();
+            ConsoleOutputView.printRace(racingGame.getCars());
+        }
+
+        ResultStatistics resultStatistics = new ResultStatistics(racingGame.getCars());
+        ConsoleOutputView.printWinners(resultStatistics.findWinners());
     }
 }
