4cc4db1be2d9392a952e3b8891497023dd1cb396
CODEMCD <psbum1609@gmail.com>
2019-11-17 20:10:47 +0900

    test: Car 객체의 가장 멀리 간 자동차 리스트를 반환하는 메서드 테스트 구현

--- src/test/java/CarsTest.java
+++ src/test/java/CarsTest.java
@@ -12,19 +12,36 @@ public class CarsTest {
     private static final CarName name2 = new CarName("car2");
     private static final CarName name3 = new CarName("car3");
 
-    private static final Car car1 = new Car(name1);
-    private static final Car car2 = new Car(name2);
-    private static final Car car3 = new Car(name3);
-
     @Test
     @DisplayName("자동차들 중 가장 멀리간 위치를 반환한다.")
     void get_max_position() {
+        Car car1 = new Car(name1);
+        Car car2 = new Car(name2);
+        Car car3 = new Car(name3);
+
         car2.move(new ForwardMoveStrategy());
         car3.move(new ForwardMoveStrategy());
         car3.move(new ForwardMoveStrategy());
         Cars cars = new Cars(Arrays.asList(car1, car2, car3));
 
         assertThat(cars.getMaxPosition()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("가장 멀리 간 자동차 리스트를 가져온다.")
+    void get_cars_on_max_position() {
+        Car car1 = new Car(name1);
+        Car car2 = new Car(name2);
+        Car car3 = new Car(name3);
+
+        car2.move(new ForwardMoveStrategy());
+        car2.move(new ForwardMoveStrategy());
+        car3.move(new ForwardMoveStrategy());
+        car3.move(new ForwardMoveStrategy());
+        Cars cars = new Cars(Arrays.asList(car1, car2, car3));
+
+        assertThat(cars.getCarsOnMaxPosition()).contains(car2, car3);
+        assertThat(cars.getCarsOnMaxPosition().size()).isEqualTo(2);
     }
 
     @Test
