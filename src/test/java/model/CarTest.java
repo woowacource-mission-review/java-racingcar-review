fd170c3933fde8da1e0a1a262ea68659dd124338
skygl <jupiter9501@gmail.com>
2019-11-22 11:09:09 +0900

    refactor: 필드 변수명 수정

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CarTest {
-    private static final String CAR_NAME = "ABC";
+    private static final String VALID_CAR_NAME = "ABC";
 
     private MoveDeterminer trueDeterminer = new MoveDeterminer(new AlwaysTrueMoveStrategy());
     private MoveDeterminer falseDeterminer = new MoveDeterminer(new AlwaysFalseMoveStrategy());
@@ -18,27 +18,27 @@ public class CarTest {
     @Test
     @DisplayName("차 생성")
     void car_create() {
-        assertDoesNotThrow(() -> new Car(CAR_NAME));
+        assertDoesNotThrow(() -> new Car(VALID_CAR_NAME));
     }
 
     @Test
     @DisplayName("기본 위치가 0인지 비교")
     void default_position_compare() {
-        Car car = new Car(CAR_NAME);
+        Car car = new Car(VALID_CAR_NAME);
         assertThat(car.matchPosition(DEFAULT_POSITION)).isTrue();
     }
 
     @Test
     @DisplayName("포지션 입력 차 생성")
     void car_create_with_position() {
-        assertDoesNotThrow(() -> new Car(CAR_NAME, 2));
+        assertDoesNotThrow(() -> new Car(VALID_CAR_NAME, 2));
     }
 
     @Test
     @DisplayName("특정 위치로 생성할 경우 위치 비교")
     void specific_position_compare() {
         int position = 2;
-        Car car = new Car(CAR_NAME, position);
+        Car car = new Car(VALID_CAR_NAME, position);
         assertThat(car.matchPosition(position)).isTrue();
     }
 
@@ -63,14 +63,14 @@ public class CarTest {
     @Test
     @DisplayName("자동차 이동 성공시 위치 증가")
     void car_move_success() {
-        Car car = new Car(CAR_NAME);
-        assertEquals(car.move(trueDeterminer), new MoveResult(CAR_NAME, DEFAULT_POSITION + 1));
+        Car car = new Car(VALID_CAR_NAME);
+        assertEquals(car.move(trueDeterminer), new MoveResult(VALID_CAR_NAME, DEFAULT_POSITION + 1));
     }
 
     @Test
     @DisplayName("자동차 이동 실패시 위치 동일한지")
     void car_move_failure() {
-        Car car = new Car(CAR_NAME);
-        assertEquals(car.move(falseDeterminer), new MoveResult(CAR_NAME, DEFAULT_POSITION));
+        Car car = new Car(VALID_CAR_NAME);
+        assertEquals(car.move(falseDeterminer), new MoveResult(VALID_CAR_NAME, DEFAULT_POSITION));
     }
 }
