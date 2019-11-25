b2a237b9fb48e6ab41e5a2724419ee4f490726fb
wbluke <pwb0127@naver.com>
2019-11-25 14:20:14 +0900

    refactor: RacingGameHistory.getNumOfGameRounds() 디미터 법칙에 따라 수정

--- src/main/java/racingcar/domain/RacingGameHistory.java
+++ src/main/java/racingcar/domain/RacingGameHistory.java
@@ -5,6 +5,7 @@ import racingcar.domain.car.RacingCars;
 import racingcar.domain.gameround.GameRound;
 
 import java.util.Map;
+import java.util.Set;
 
 public class RacingGameHistory {
 
@@ -19,6 +20,7 @@ public class RacingGameHistory {
     }
 
     public long getNumOfGameRounds() {
-        return history.keySet().size();
+        Set<GameRound> gameRounds = history.keySet();
+        return gameRounds.size();
     }
 }
